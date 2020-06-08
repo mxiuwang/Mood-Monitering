09945fd539370e44b414da89b8f022bc5cb49219
mxiuwang <mxwang@ualberta.ca>
2020-06-08 06:55:26 -0600

    submit an activity page

--- app/src/main/java/com/example/moodtracker/MainActivity.java
+++ app/src/main/java/com/example/moodtracker/MainActivity.java
@@ -1,24 +1,28 @@
 package com.example.moodtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-    public void clickRecordAnActivity(View view) { // button object is of type View
-
-        EditText hoursSleptEntryHomepage = (EditText) findViewById(R.id.hoursSleptEntryHomepage); // findViewByID returns a view, which we cast to EditText object
-
-        Log.i("Info", "RecordAnAcitivity button pressed"); // optiona: displays action in logs
-        Log.i("Values", hoursSleptEntryHomepage.getText().toString()); // displays text entered in logs
-    }
 
     @Override // overrides the onCreate method in the parent class (AppCompatActivity)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // super refers to onCreate method that exists in AppCompatActivity
         setContentView(R.layout.activity_main); // runs activity_main.xml interface
+
+    }
+
+    public void clickRecordAnActivity(final View view) { // button object is of type View
+
+        Intent intent = new Intent(view.getContext(), MakeEntry.class);
+        startActivity(intent);
+
+        Log.i("Info", "RecordAnAcitivity button pressed"); // optiona: displays action in logs
     }
 }
--- app/src/main/java/com/example/moodtracker/MakeEntry.java
+++ app/src/main/java/com/example/moodtracker/MakeEntry.java
@@ -3,6 +3,9 @@ package com.example.moodtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MakeEntry extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MakeEntry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_entry);
+    }
+
+    public void clickSubmit (View view) {
+        EditText hoursSleptEntry = (EditText) findViewById(R.id.hoursSleptEntry); // findViewByID returns a view, which we cast to EditText object
+        Log.i("Values", hoursSleptEntry.getText().toString()); // displays text entered in logs
+
     }
 }
