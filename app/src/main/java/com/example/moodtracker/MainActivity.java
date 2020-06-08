a9dd2d9cc3ada836de9a0686f754a7c3c12e2d58
mxiuwang <mxwang@ualberta.ca>
2020-06-08 02:12:02 -0600

    homepage design

--- app/src/main/java/com/example/moodtracker/MainActivity.java
+++ app/src/main/java/com/example/moodtracker/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.moodtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    public void clickRecordAnActivity(View view) { // button object is of type View
+
+        EditText hoursSleptEntryHomepage = (EditText) findViewById(R.id.hoursSleptEntryHomepage); // findViewByID returns a view, which we cast to EditText object
+
+        Log.i("Info", "RecordAnAcitivity button pressed"); // optiona: displays action in logs
+        Log.i("Values", hoursSleptEntryHomepage.getText().toString()); // displays text entered in logs
+    }
+
+    @Override // overrides the onCreate method in the parent class (AppCompatActivity)
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState); // super refers to onCreate method that exists in AppCompatActivity
+        setContentView(R.layout.activity_main); // runs activity_main.xml interface
     }
 }
---
+++ app/src/main/java/com/example/moodtracker/MakeEntry.java
@@ -0,0 +1,14 @@
+package com.example.moodtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakeEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_entry);
+    }
+}
